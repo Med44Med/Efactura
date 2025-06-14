361f85d0e83582f557e51b22becec6dcbe304ef6
med44am <mstgci4@gmail.com>
2025-06-14 16:52:17 +0100

    SideBar 1

--- Website/src/App.tsx
+++ Website/src/App.tsx
@@ -1,12 +1,17 @@
 import "./App.css";
 import Home from "./routes/home";
 import { BrowserRouter, Routes, Route } from "react-router";
+import Dashboard from "./routes/dashboard";
+import Invoice from './routes/invoice';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route index element={<Home />} />
+        {/* <Route index element={<Home />} /> */}
+        <Route element={<Dashboard />}>
+          <Route index element={<Invoice />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ Website/src/routes/invoice.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Invoice = () => {
+  return (
+    <div className='text-black'>Invoice</div>
+  )
+}
+
+export default Invoice
